9e252f5ee776ac2d5172d7394b16d7c84506776c
emorgan316 <elliemorgan96@gmail.com>
2021-06-03 18:13:17 -0400

    fixed search and got music to play

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-Const url = 'https://proxy-itunes-api.glitch.me/search?'
+
+
+const url = 'https://itunes.apple.com/search?'
 const form = document.getElementById('input-field')
-const searchInput = document.getElementById('search-input').value
+const container = document.querySelector('.container')
+const searchInput = document.getElementById('search-input')
 searchResults = document.getElementById('output')
 
 document.addEventListener('submit', event => {
     event.preventDefault()
-    createNewSearch()
+    searchArtist()
 });
 
-function createNewSearch() {
-    //making request to the url for the search
-    fetch(url + 'term=' + `${searchInput}` + '&media=music' + '&entity=song' + '&limit=10')
-    //returns the request in json format
+function searchArtist() {
+    const search = searchInput.value
+    fetch(url + 'term=' + `${search}` + '&entity=song' + '&limit=10')
     .then(response =>  response.json())
-    //looping through data
     .then((data) => {
-        console.log(data)
-        console.log(url + 'term=' + `${searchInput}` + '&media=music' + '&limit=10')
+
+        document.getElementById('output').innerHTML = '';
         for (let item of data.results) {
             console.log(item)
             renderResults(item)
@@ -24,12 +25,31 @@ function createNewSearch() {
     })
 };
 
-function renderResults(musicObj) {
+function renderResults(data) {
+    
     let eachResult = document.createElement('li')
+    eachResult.id = data.trackId
     searchResults.appendChild(eachResult)
+
+    let trackName = document.createElement('div')
+    trackName.setAttribute('id', 'track-name')
+    eachResult.appendChild(trackName)
+    trackName.innerHTML = `${data.trackName}`
+
+    let trackArt = document.createElement('img')
+    trackArt.setAttribute('id', 'track-image')
+    eachResult.appendChild(trackArt)
+    trackArt.src = `${data.artworkUrl60}`
 
     let artist = document.createElement('div')
     artist.setAttribute('id', 'artist-name')
     eachResult.appendChild(artist)
-    artist.innerHTML = `${musicObj.artistName}`
+    artist.innerHTML = `${data.artistName}`
+
+        let audio = document.createElement('audio')
+        audio.id = 'audio-preview'
+        audio.controls = 'controls'
+        audio.src = `${data.previewUrl}`
+        eachResult.appendChild(audio)
+
 }
